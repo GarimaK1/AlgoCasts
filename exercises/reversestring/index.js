01e23806b50572efa42cfb915db51f1156c64d30
Garima <garima.k.03@gmail.com>
2020-07-12 11:38:32 -0400

    Reversedstring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,34 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((rev, ch) => ch + rev, '');
+}
+
+reverse('reverse');
+
+// function reverse(str) {
+//     let revStr = '';
+//     for (let i = str.length-1 ; i >= 0; i--) {
+//         revStr = revStr + str.charAt(i);
+//     }
+//     return revStr;
+// }
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// function reverse(str) {
+//     let reversed = '';
+//     for (let c of str) {
+//         reversed = c + reversed;
+//     }
+//     return reversed;
+// }
 
 module.exports = reverse;
